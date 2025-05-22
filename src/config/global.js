9ecd9babd9e267edf53ba9a4400c099bf36e63b4
Robinson Ordonez <robinsonordonezbarreiro@gmail.com>
2025-05-22 10:08:40 -0500

    Vinculación DU-PDF - MD

--- src/config/global.js
+++ src/config/global.js
@@ -168,7 +168,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/dist.pdf',
+        download: 'downloads/123101_CF14_DU.zip',
       },
       {
         icono: 'fas fa-download',
